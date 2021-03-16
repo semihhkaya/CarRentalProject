e55b8aba74b175180b8373da77691f11bc18cf57
DELL <DELL@SEMIH>
2021-03-16 12:35:00 +0300

    Add project files.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -9,10 +9,27 @@ namespace Business.Concrete
 {
     public class CarManager:ICarService
     {
-        ICarDal _carDal;
+        ICarDal _carDal; //Dependency Injection
         public CarManager(ICarDal carDal)
+        {                       
+            _carDal = carDal; //Dependency Injection
+        }
+
+        public void Add(Car car)
         {
-            _carDal = carDal;
+            if (car.CarName.Length > 2 && car.DailyPrice > 0)
+            {
+                _carDal.Add(car);
+            }
+            else
+            {
+                Console.WriteLine("Araba Adı 2 karakterden az olamaz. Ve Günlük Fiyat 0'dan büyük olmalıdır.");
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
         }
 
         public List<Car> GetAll()
@@ -20,9 +37,19 @@ namespace Business.Concrete
             return _carDal.GetAll();
         }
 
-        public List<Car> GetById()
+        public List<Car> GetCarsByBrandId(int id)
         {
-            throw new NotImplementedException();
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
         }
     }
 }
